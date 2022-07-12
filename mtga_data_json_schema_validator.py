24524c34fd7d1639d06557d280aeff7e898b3170
poslogithub <black_tp@hotmail.com>
2022-07-13 06:14:03 +0900

    Rawに対応（SQLite対応はまだ）

--- mtga_data_json_schema_validator.py
+++ mtga_data_json_schema_validator.py
@@ -1,17 +1,20 @@
 import sys
 import glob
 import json
-from jsonschema import validate, ValidationError
+from os.path import join
+
+from jsonschema import validate, ValidationError    # The source material in this repository is licensed under the AFL or BSD license.
 
 if len(sys.argv) >= 2:
     json_dir = sys.argv[1]
 else:
-    json_dir = r'C:\\Program Files\\Wizards of the Coast\\MTGA\\MTGA_Data\\Downloads\\Data'
+    json_dir = r'C:\Program Files\Wizards of the Coast\MTGA\MTGA_Data\Downloads\Raw'
 
 if len(sys.argv) >= 3:
     schema_dir = sys.argv[2]
 else:
     schema_dir = r'json-schema'
+
 
 mtga_files = glob.glob(json_dir+"\\*.mtga")
 for mtga_file in mtga_files:
@@ -24,10 +27,14 @@ for mtga_file in mtga_files:
         try:
             json_obj = json.load(json_file)
         except json.JSONDecodeError as e:
-            print(e.msg)
+            print(e)
+            continue
+        except UnicodeDecodeError as e:
+            print(e)
             continue
 
-    with open(schema_dir+"\\"+schema_file_name, encoding="utf_8_sig") as schema_file:
+    schema_path = join(schema_dir, schema_file_name)
+    with open(schema_path, encoding="utf_8_sig") as schema_file:
         schema_obj = json.load(schema_file)
 
     try:
